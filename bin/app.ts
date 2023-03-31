9f5190a1e35c9ff32cb9080145f185b1837b62a0
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-03-31 09:34:02 -0400

    add new stack to app

--- bin/app.ts
+++ bin/app.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import 'source-map-support/register';
-import { WikiVizStack } from '../lib/stacks/wikiviz';
+import * as cdk from 'aws-cdk-lib'
+import 'source-map-support/register'
+import { WebSocketApiStack } from '../lib/stacks/websocket'
+import { WikiVizStack } from '../lib/stacks/wikiviz'
 
-const app = new cdk.App();
-new WikiVizStack(app, 'WikiVizApi', {
-  //
-});
+const app = new cdk.App()
+new WikiVizStack(app, 'WikiVizApi')
+
+new WebSocketApiStack(app, 'WebSocketApi')
