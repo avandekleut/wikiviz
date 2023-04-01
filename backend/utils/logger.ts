16fb0e70b3bbffb299d48b37573667ec88e47cee
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-01 10:45:11 -0400

    remove deprecated prettyprint

--- backend/utils/logger.ts
+++ backend/utils/logger.ts
@@ -1,18 +1,17 @@
-import pino from 'pino';
+import pino from 'pino'
 
 function init() {
   return pino({
-    prettyPrint: true,
     level: process.env.LOG_LEVEL || 'info',
     base: { requestId: '' },
     timestamp: false,
-  });
+  })
 }
 
 export class LoggerFactory {
-  public static logger = init();
+  public static logger = init()
 
   public static setRequestId(requestId: string): void {
-    LoggerFactory.logger = init().child({ requestId });
+    LoggerFactory.logger = init().child({ requestId })
   }
 }
