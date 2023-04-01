28ce79d9a70b137e3d191edb4fff120c63870491
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-01 10:24:52 -0400

    working with websockets

--- frontend/src/hooks/useWebsocket.ts
+++ frontend/src/hooks/useWebsocket.ts
@@ -8,20 +8,20 @@ export const useWebSocket = (url: string) => {
     const ws = new WebSocket(url)
 
     ws.onopen = () => {
-      console.log('Connected to WebSocket')
+      console.log('Connected to WebSocket', new Date().valueOf())
     }
 
     ws.onmessage = (event) => {
-      console.log('Received message:', event.data)
+      console.log('Received message:', event.data, new Date().valueOf())
       setMessages((prevMessages) => [...prevMessages, event.data])
     }
 
     ws.onerror = (event) => {
-      console.error('WebSocket error:', event)
+      console.error('WebSocket error:', event, new Date().valueOf())
     }
 
     ws.onclose = () => {
-      console.log('Disconnected from WebSocket')
+      console.log('Disconnected from WebSocket', new Date().valueOf())
       setSocket(null)
     }
 
@@ -29,7 +29,11 @@ export const useWebSocket = (url: string) => {
 
     // Clean up the WebSocket connection when the component unmounts
     return () => {
-      ws.close()
+      console.log('Cleaning up WebSocket connection', new Date().valueOf())
+
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.close()
+      }
     }
   }, [url])
 
