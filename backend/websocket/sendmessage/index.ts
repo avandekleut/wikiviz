c968113b16e2bb9ac52b1aa836b089d0c91732ec
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-01 21:31:01 -0400

    run crawler in websocket

--- backend/websocket/sendmessage/index.ts
+++ backend/websocket/sendmessage/index.ts
@@ -1,6 +1,25 @@
 import { APIGatewayProxyWebsocketHandlerV2 } from 'aws-lambda'
 import { ApiGatewayManagementApi } from 'aws-sdk'
+import { Crawler, CrawlerCallback } from '../../utils/crawler'
+import { Graph } from '../../utils/graph'
+import { HttpError } from '../../utils/http-error'
+import { LoggerFactory } from '../../utils/logger'
 import { CORS_HEADERS } from '../cors'
+
+type OptionalProperties<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export type CrawlRequest = {
+  wikid: string
+  depth: number
+  branchingFactor: number
+}
+
+export type CrawlParams = OptionalProperties<
+  CrawlRequest,
+  'depth' | 'branchingFactor'
+>
+
+type Result = OptionalProperties<CrawlRequest, 'depth' | 'branchingFactor'>
 
 export const handler: APIGatewayProxyWebsocketHandlerV2 = async (
   event,
@@ -16,37 +35,52 @@ export const handler: APIGatewayProxyWebsocketHandlerV2 = async (
     endpoint: endpoint,
   })
 
+  if (event.body === undefined) {
+    throw new HttpError(400, `Missing body.`)
+  }
+  const data: CrawlParams = JSON.parse(event.body)
+  const wikid = data['wikid']
+  const depth = data['depth']
+  const branchingFactor = data['branchingFactor']
+
+  const graph = new Graph()
+  const crawler = new Crawler()
+
   try {
-    const callback = async (iter: number) => {
-      const payload = {
-        message: `Message ${iter}`,
-      }
+    const callback: CrawlerCallback = async (pageData) => {
+      graph.nodes.add(graph.pageDataToNode(pageData))
 
-      // Sleep for a short time to simulate processing
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-
-      // Send the message to the client
       await apiGatewayManagementApi
         .postToConnection({
           ConnectionId: connectionId,
-          Data: JSON.stringify(payload),
+          Data: JSON.stringify(pageData),
         })
         .promise()
+
+      for (const child of pageData.children) {
+        graph.edges.add({
+          from: wikid,
+          to: child,
+          id: `${wikid} -> ${child}`,
+        })
+      }
     }
 
-    // Send multiple messages back to the client
-    for (let i = 1; i <= 10; i++) {
-      await callback(i)
-    }
+    await crawler.crawl(wikid, { callback, depth, branchingFactor })
 
-    // Return a successful response to the client
+    LoggerFactory.logger.info(`Finished crawling ${wikid} with depth ${depth}`)
+
     return {
       statusCode: 200,
       body: 'Messages sent successfully.',
       headers: CORS_HEADERS,
     }
   } catch (error) {
-    console.error(`Failed to send messages to client: ${error}`)
+    LoggerFactory.logger.error({
+      error,
+      depth,
+      msg: `Error while crawling ${wikid} with depth ${depth}:`,
+    })
 
     // Return an error response to the client
     return {
