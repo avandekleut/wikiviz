b4765c92d1ad7dd1d8f67ec4a419c2bf4d1810e2
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-04 21:14:03 -0400

    use in-memory cache for crawler

--- backend/utils/crawler.ts
+++ backend/utils/crawler.ts
@@ -1,5 +1,5 @@
 import { PageData } from './pagedata'
-import { wikipediaSummaryAndLinks } from './wiki'
+import { wikipediaSummaryAndLinks as getWikipediaSummaryAndLinks } from './wiki'
 
 export type CrawlerCallback = (data: PageData) => void | Promise<void>
 
@@ -8,6 +8,8 @@ type CrawlerParams = {
   branchingFactor?: number
   callback?: CrawlerCallback
 }
+
+const visitedWikids: Record<string, PageData> = {}
 
 export class Crawler {
   async crawl(
@@ -22,11 +24,25 @@ export class Crawler {
       return
     }
 
-    const pageData = await wikipediaSummaryAndLinks(wikid)
+    let pageData: PageData
 
+    if (visitedWikids[wikid]) {
+      // fetch pageData from in-memory cache
+      console.log({ wikid, msg: 'cache hit' })
+      pageData = visitedWikids[wikid]
+      return
+    } else {
+      // fetch pageData fresh from wikipedia
+      console.log({ wikid, msg: 'cache hit' })
+      pageData = await getWikipediaSummaryAndLinks(wikid)
+    }
+
+    // run callback only for first visit
     if (callback) {
       callback(pageData)
     }
+
+    visitedWikids[wikid] = pageData
 
     const children = pageData.children
     for (const child of children.slice(0, branchingFactor)) {
