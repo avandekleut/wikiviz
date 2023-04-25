8879be9d514f1cfaf021bbde3396e48807721950
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-24 21:52:58 -0400

    get image from infobox

--- backend/utils/wiki.ts
+++ backend/utils/wiki.ts
@@ -3,10 +3,6 @@ import { URL } from 'url'
 import { fetchDataWithRetries } from './fetchWithBackoff'
 import { LoggerFactory } from './logger'
 import { PageData } from './pagedata'
-
-// TODO: remove this
-const apiKey =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmOGVjZGUxMzFkY2Q1Y2E5ZDJlNjZlNmU4YzEzZjExMiIsImp0aSI6Ijg5ZjY5OTc0YmQ4M2FhNzgwMzg1ODlhMmEzZWEwZDhjNTgwNDE2YzBkMmYzNjlhYzg3Njk4YjQyMzdhZTNhYmM0ZWZhYjZiOGRkNGRkY2IxIiwiaWF0IjoxNjgwMzY1MjAwLjgxOTY5MiwibmJmIjoxNjgwMzY1MjAwLjgxOTY5NiwiZXhwIjozMzIzNzI3NDAwMC44MTc2ODQsInN1YiI6IjcyNDQwMjU1IiwiaXNzIjoiaHR0cHM6Ly9tZXRhLndpa2ltZWRpYS5vcmciLCJyYXRlbGltaXQiOnsicmVxdWVzdHNfcGVyX3VuaXQiOjUwMDAsInVuaXQiOiJIT1VSIn0sInNjb3BlcyI6WyJiYXNpYyJdfQ.lvZwHn9hqvOGG0Mys6p47SZ1r_f-0OQmc2-fy9QxR0Gim7TpV6YHCcvPtXn_XYW5V21kAfpoPS4CAnmx7jzeG-3vN3mrgvxq8gHjmPY0v3po6JYuFEPocStklCEg7prYcFrup968KrFC1LbLQDjQ3a_0l-XYyplF3LG7obM0PXY0DEyA9uCRNHbBlI0kDYIikdl0XBehfKbqCOK9TsvegMt3iZZ3rNVH-UgFNqMJIg1m6kfcMdQOWmh6kVoSmF2BXBgWsnGvzJg-lcAS6rflYtAOCSTQYnIpK23Z-3-U7XReRvRhLr0uxR6wzeP0uuoG2XtDwuUKS86BxD9eNVNCj1ilKmZrNY5M-daAU1yEp1qIuTM0I3TO8BBrQPs0RL5vtOo75ZS5zcFlfdRoxcrRNS8qDkoGZz-8TbIy0J3JFmynBBNpBTxIFccFmXUxkocFPEXOjD7v7IzBiQhTRtSQsMV3TQDlCoS6sU6Qus1krL3EOK7zTvED5Apx7H_lmz73mH2skwzWiEyCHrdIJo3tzIbrjiK65OpZSCcdiCuTkZkWLsJNfIpmrdmBhouCFSAfIhhiBC2bbVa7yAdJakXI97ZKOFXbr7ilBjCNECwf41faTFOs29M5wWcpZiAxKhtyGg8thZzhCLR4ab47hrFVzDhGIINH5GI9RhnYIy5kYRY'
 export async function wikipediaSummaryAndLinks(
   title: string,
 ): Promise<PageData> {
@@ -37,7 +33,8 @@ export async function wikipediaSummaryAndLinks(
   let mainImage: string | undefined
   const infobox = $('table.infobox').first()
   if (infobox.length > 0) {
-    mainImage = infobox.find('img').first().attr('src')
+    const tbody = infobox.find('tbody').first()
+    mainImage = tbody.find('img').first().attr('src')
   }
 
   const childArticles = $('div.mw-parser-output a[href^="/wiki/"]')
