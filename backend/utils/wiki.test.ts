74321dc8af53be6961c7d9a588f997e673f580f1
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-04-24 22:35:16 -0400

    test for Moon instead of Functor

--- backend/utils/wiki.test.ts
+++ backend/utils/wiki.test.ts
@@ -5,9 +5,7 @@ jest.setTimeout(30_000)
 describe('wiki', () => {
   it('should get summary and links', async () => {
     console.time('wikipediaSummaryAndLinks')
-    const { summary, children: links } = await wikipediaSummaryAndLinks(
-      'Functor',
-    )
+    const { summary, children: links } = await wikipediaSummaryAndLinks('Moon')
     console.timeEnd('wikipediaSummaryAndLinks')
     console.log({ summary, links })
   })
